15e526d7d567e10214837b9c2b81412587f3c67a
allcallsmeprakash <154727203+allcallsmeprakash@users.noreply.github.com>
2025-08-06 18:30:31 +0530

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    department = db.Column(db.String(100))
+    contact = db.Column(db.String(100))
